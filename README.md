cab4e8742f400ef3dc0318d5e4a4a1d1f48ce856
Joshua Hawxwell <m@hawx.me>
2022-03-08 14:39:36 +0000

    Add common packages (#1)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # opg-go-common
+
+[![codecov](https://codecov.io/gh/ministryofjustice/opg-go-common/branch/main/graph/badge.svg?token=BFGR5FBQ0T)](https://codecov.io/gh/ministryofjustice/opg-go-common)
+[![PkgGoDev](https://pkg.go.dev/badge/github.com/ministryofjustice/opg-go-common)](https://pkg.go.dev/github.com/ministryofjustice/opg-go-common)
+
 Common go packages used by OPG: Managed by opg-org-infra &amp; Terraform
---
+++ securityheaders/use.go
@@ -0,0 +1,17 @@
+package securityheaders
+
+import "net/http"
+
+// Use applies common security headers to a handler.
+func Use(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Security-Policy", "default-src 'self'")
+		w.Header().Add("Referrer-Policy", "same-origin")
+		w.Header().Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+		w.Header().Add("X-Content-Type-Options", "nosniff")
+		w.Header().Add("X-Frame-Options", "SAMEORIGIN")
+		w.Header().Add("X-XSS-Protection", "1; mode=block")
+
+		h.ServeHTTP(w, r)
+	}
+}
